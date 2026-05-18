d662abff060a0aa5acaf085102fbe792ea9d0b7f
jrodriguezr49-pixel <jrodriguezr49@miumg.edu.gt>
2026-05-17 23:21:52 -0600

    Add files via upload

---
+++ Tarea_tablas_hash.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_tablas_hash;
+
+/**
+ *
+ * @author javie
+ */
+public class Tarea_tablas_hash {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
